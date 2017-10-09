5b5ab5deaecd6924af8411ffe933f3344dcdb64f
bengarvey <bgarvey@rjmetrics.com>
2017-10-08 23:02:39 -0400

    added axes

--- index.js
+++ index.js
@@ -25,27 +25,6 @@ var deathDisplay = [
   {data: modified.deaths, color: colors.deaths, renderMode: "normal"}
 ];
 
-const popAnnotations = [
-  { type: 'x', x: yearToDate(1968),
-    note: { label: "Seat belts required", align: "middle", wrap: 50},
-    color: colors.annotation, dy: -10, dx: 0, connector: { end: "none" } },
-  { type: "x", x: yearToDate(1984),
-    note: { label: "NY seat belt law", align: "middle", wrap: 100},
-    color: colors.annotation, dy: -5, dx: 0, connector: { end: "none" } },
-  { type: "x", x: yearToDate(1994),
-    note: { label: "Seat belt laws: 90% of states", align: "middle", wrap: 500},
-    color: colors.annotation, dy: -20, dx: 0, connector: { end: "none" } },
-  { type: "x", x: yearToDate(1998),
-    note: { label: "Airbags required", align: "middle", wrap: 500},
-    color: colors.annotation, dy: -5, dx: 35, connector: { end: "none" } },
-  { type: "x", x: new Date("1945-08-15"),
-    note: { label: "WW2 Ends", align: "middle", wrap: 500},
-    color: colors.annotation, dy: -20, dx: 0, connector: { end: "none" } },
-  { type: "x", x: new Date("1941-12-07"),
-    note: { label: "WW2 Begins", align: "right", wrap: 500},
-    color: colors.annotation, dy: -5, dx: 0, disable:["connector"] }
-];
-
 var sharedProps = {
   size: [1000,400],
   xAccessor: "x",
@@ -65,7 +44,6 @@ ReactDOM.render(
     lineRenderMode={d => d.renderMode}
     lineStyle={d => ({stroke: d.color, strokeWidth: "2px" })}
     customLineType={{ type: "dividedLine"}}
-    annotations={popAnnotations}
     axes={[
       { orient: 'bottom', ticks: 8, tickFormat: d => new Date(d).getFullYear() },
       { orient: 'left', ticks: 10, tickFormat: d => d.toLocaleString()}
